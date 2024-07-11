e32aee090ed8e216261c910b8a4c7f9b6c81c250
SemirSalkic <semirsalkic2439@gmail.com>
2024-07-12 01:09:22 +0200

    Update border size in CalenderCard

--- src/components/CalendarCard.vue
+++ src/components/CalendarCard.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
   <div
-    class="p-2 border lg:w-32 lg:h-28 md:w-24 md:h-22 sm:w-16 sm:h-22 rounded-xl"
+    class="p-2 border-2 lg:w-32 lg:h-28 md:w-24 md:h-22 sm:w-16 sm:h-22 rounded-xl"
     :class="{ 'bg-gray-200 text-black': isWeekend }"
   >
     <div class="lg:text-lg md:text-base sm:text-sm font-bold">{{ day }}</div>
