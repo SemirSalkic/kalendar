1cb62e609fc1e4abe72ecdc2384d436c69db6230
SemirSalkic <semirsalkic2439@gmail.com>
2024-07-11 13:38:29 +0200

    add calenderView to app and create darkMode

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isDark = useDark()
+</script>
 
 <template>
-  <main
-    class="flex h-full max-h-full w-full min-w-min flex-col bg-white text-black dark:bg-black dark:text-white"
-  ></main>
+  <main class="relative bg-white text-black dark:bg-black dark:text-white p-10 h-full">
+    <div class="absolute top-0 right-0 p-4">
+      <VSwitch v-model="isDark"></VSwitch>
+    </div>
+    <div class="flex flex-col items-center justify-center h-full">
+      <div class="border-2 p-4 flex flex-col items-center rounded-xl">
+        <div class="flex justify-between w-full mb-4">
+          <TitleComponent title="Kalendar"></TitleComponent>
+          <VButton color="neutral" label="Registruj odsustvo"></VButton>
+        </div>
+        <CalenderView></CalenderView>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
