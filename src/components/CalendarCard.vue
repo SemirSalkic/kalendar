0fbe417b7d47133b90888a6f7a38f7172a39981e
SemirSalkic <semirsalkic2439@gmail.com>
2024-07-11 13:36:37 +0200

    Create CalendarCard.vue

---
+++ src/components/CalendarCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  day: number
+  description: string
+  isWeekend: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="p-2 border lg:w-32 lg:h-28 md:w-24 md:h-22 sm:w-16 sm:h-22 rounded-xl"
+    :class="{ 'bg-gray-200 text-black': isWeekend }"
+  >
+    <div class="lg:text-lg md:text-base sm:text-sm font-bold">{{ day }}</div>
+    <div class="sm:text-sm text-xs">{{ description }}</div>
+  </div>
+</template>
