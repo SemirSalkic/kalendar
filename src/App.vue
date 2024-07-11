3689e8093697c954587ac0fcfb97654f95b9632d
SemirSalkic <semirsalkic2439@gmail.com>
2024-07-11 11:09:37 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main
+    class="flex h-full max-h-full w-full min-w-min flex-col bg-white text-black dark:bg-black dark:text-white"
+  ></main>
+</template>
+
+<style scoped></style>
