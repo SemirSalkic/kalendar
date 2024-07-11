0f6ff2b8b9c2d023475a67494b97a8cbe92ab8a0
SemirSalkic <semirsalkic2439@gmail.com>
2024-07-11 11:51:00 +0200

    add auto-import, heroicons, headlessui, vueuse

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -9,6 +11,16 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      dirs: ['src/components', 'src/views'],
+      dts: 'src/components.d.ts'
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'vue-i18n', '@vueuse/core', '@vueuse/head'],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/stores'],
+      vueTemplate: true
+    })
   ],
   resolve: {
     alias: {
